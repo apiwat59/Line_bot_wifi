edd70c02dab88498376438e600d4ba242aea0e85
Nattapon6595 <47565548+Nattapon6595@users.noreply.github.com>
2019-02-14 16:53:20 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -1,19 +1,23 @@
+
 #include <ESP8266WiFi.h>
 #include <MicroGear.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const char* ssid     = "your SSID"; //change this to your SSID
-const char* password = "your PASSWORD"; //change this to your PASSWORD
 
-const char* host = "your linebot server";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "your APPID"     //change this to your APPID
-#define KEY     "your KEY"     //change this to your KEY
-#define SECRET  "your SECRET"     //change this to your SECRET
+const char* ssid     = "Niramon_2.4GHz";
+const char* password = "0979796595";
 
-#define ALIAS   "NodeMCU1" //set name of drvice
-#define TargetWeb "switch" //set target name of web
+const char* host = "http://wificontrolpower.herokuapp.com/bot.php";
+
+#define APPID   "linebotto8266"
+#define KEY     "HuUOn5Zff4Wnkyj"
+#define SECRET  "mRiozkdhpw7JivDezh8fCKVdq"
+#define ALIAS   "NodeMCU1"   /////"esp8266"///
+#define TargetWeb "switch".
+
 
 WiFiClient client;
+
 String uid = "";
 int timer = 0;
 MicroGear microgear(client);
@@ -67,7 +71,7 @@ Serial.println("WiFi connected");
 }
 
 void send_json(String data){
-  StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
+    StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer///
     JsonObject& JSONencoder = JSONbuffer.createObject(); 
  
     JSONencoder["ESP"] = data;
