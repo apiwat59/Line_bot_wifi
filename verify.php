39d378c0eda2451b178894208a78f59c08800ce7
Nattapon6595 <47565548+Nattapon6595@users.noreply.github.com>
2019-02-14 16:29:25 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'LG03eF7ZQAq1J1vTACiQP2h/1W98nGw65OTkKwS13RzzlfDh5YGcUvTib6/pc7kFIQKQArO5MYvxjJSUwxNQwk+ToGcABsDYuFvsGuEUusIyvlMG8UZF5Xr4knytfILv8G7WMFutDUPu2WbOq0ueTgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
